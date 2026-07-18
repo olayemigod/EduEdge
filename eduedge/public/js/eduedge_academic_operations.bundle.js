8379cd30d662c2a3ae66b26caf917ca7d470e5d3
olayemigod <olayemigod@gmail.com>
2026-07-18 07:35:57 +0100

    feat(eduedge): add Academic Operations bundle entry

---
+++ eduedge/public/js/eduedge_academic_operations.bundle.js
@@ -0,0 +1,7 @@
+import EduEdgeAcademicOperations from "./eduedge_academic_operations/EduEdgeAcademicOperations.vue";
+
+if (typeof window !== "undefined") {
+	window.EduEdgeAcademicOperations = EduEdgeAcademicOperations;
+}
+
+export default EduEdgeAcademicOperations;
