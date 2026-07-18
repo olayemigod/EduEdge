abce9686ee474efad50fd2b564d3cb171adcbbb8
olayemigod <olayemigod@gmail.com>
2026-07-18 17:44:33 +0100

    feat(eduedge): add branch governance center

---
+++ eduedge/public/js/eduedge_branch_governance.bundle.js
@@ -0,0 +1,7 @@
+import EduEdgeBranchGovernance from "./eduedge_branch_governance/EduEdgeBranchGovernance.vue";
+
+if (typeof window !== "undefined") {
+	window.EduEdgeBranchGovernance = EduEdgeBranchGovernance;
+}
+
+export default EduEdgeBranchGovernance;
