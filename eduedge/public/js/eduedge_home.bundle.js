e7067cc6fadffebd2d03df666680b53c4d0657e6
olayemigod <olayemigod@gmail.com>
2026-07-19 20:05:00 +0100

    fix(eduedge): expose home app factory

--- eduedge/public/js/eduedge_home.bundle.js
+++ eduedge/public/js/eduedge_home.bundle.js
@@ -1,7 +1,13 @@
 import EduEdgeHome from "./eduedge_home/EduEdgeHome.vue";
+import { createEduEdgeApp } from "./eduedge_ui/app_factory";
+
+export function createEduEdgeHomeApp(rootProps = null) {
+	return createEduEdgeApp(EduEdgeHome, rootProps);
+}
 
 if (typeof window !== "undefined") {
 	window.EduEdgeHome = EduEdgeHome;
+	window.createEduEdgeHomeApp = createEduEdgeHomeApp;
 }
 
 export default EduEdgeHome;
