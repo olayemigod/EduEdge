b2594ac2567d22c13c609b06e087575bd7276e6a
olayemigod <olayemigod@gmail.com>
2026-07-19 20:05:20 +0100

    fix(eduedge): expose assessment operations app factory

--- eduedge/public/js/eduedge_assessment_operations.bundle.js
+++ eduedge/public/js/eduedge_assessment_operations.bundle.js
@@ -1,7 +1,13 @@
 import EduEdgeAssessmentOperations from "./eduedge_assessment_operations/EduEdgeAssessmentOperations.vue";
+import { createEduEdgeApp } from "./eduedge_ui/app_factory";
+
+export function createEduEdgeAssessmentOperationsApp(rootProps = null) {
+	return createEduEdgeApp(EduEdgeAssessmentOperations, rootProps);
+}
 
 if (typeof window !== "undefined") {
 	window.EduEdgeAssessmentOperations = EduEdgeAssessmentOperations;
+	window.createEduEdgeAssessmentOperationsApp = createEduEdgeAssessmentOperationsApp;
 }
 
 export default EduEdgeAssessmentOperations;
