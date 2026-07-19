f02e5495d13985aca80b0bc7b5ace6aa29ced7f5
olayemigod <olayemigod@gmail.com>
2026-07-19 23:37:15 +0100

    feat(eduedge): register programme offerings resource page

---
+++ eduedge/eduedge/page/eduedge_program_offerings/eduedge_program_offerings.js
@@ -0,0 +1,6 @@
+window.registerEduEdgeResourcePage({
+	pageName: "eduedge-program-offerings",
+	title: "Programme Offerings",
+	resourceKey: "program_offerings",
+	activeRoute: "/app/eduedge-program-offerings",
+});
