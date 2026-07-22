52a347e3da2162c5cad483729e35a72752a8edc2
olayemigod <olayemigod@gmail.com>
2026-07-22 21:27:57 +0100

    integrate(eduedge): combine CBT and institution hooks

--- eduedge/hooks.py
+++ eduedge/hooks.py
@@ -8,6 +8,7 @@ app_license = "mit"
 required_apps = ["erpnext", "education", "edgesuite_ui"]
 app_include_css = []
 app_include_js = [
+	"eduedge_terminology.bundle.js",
 	"eduedge_product_menu.bundle.js",
 	"eduedge_resource_page_loader.bundle.js",
 ]
@@ -29,6 +30,7 @@ override_whitelisted_methods = {
 	"eduedge.api.resource_center.save_resource_record": "eduedge.api.resource_center_safe.save_resource_record",
 	"eduedge.api.resource_center.delete_resource_record": "eduedge.api.resource_center_safe.delete_resource_record",
 	"eduedge.api.modal_records.save_modal_record": "eduedge.api.modal_records_safe.save_modal_record",
+	"eduedge.api.academic_operations.get_operations_context": "eduedge.api.academic_operations_safe.get_operations_context",
 }
 
 add_to_apps_screen = [
@@ -36,13 +38,13 @@ add_to_apps_screen = [
 		"name": "eduedge",
 		"logo": "/assets/eduedge/images/eduedge-mark.svg",
 		"title": "EduEdge",
-		# Frappe v16 recognises /desk routes as internal Desk apps. Using the
-		# legacy /app alias makes the launcher treat EduEdge as an external app.
 		"route": "/desk/eduedge-home",
 	}
 ]
 
 doctype_js = {
+	"Program": "public/js/education/program.js",
+	"Course": "public/js/education/course.js",
 	"Student Admission": "public/js/education/student_admission.js",
 	"Student Applicant": "public/js/education/student_applicant.js",
 	"Student": "public/js/education/student.js",
@@ -56,6 +58,30 @@ doctype_js = {
 }
 
 doc_events = {
+	"Company": {
+		"before_validate": "eduedge.education.institution_types.before_validate_company",
+	},
+	"Program": {
+		"before_validate": "eduedge.education.academic_validation.before_validate_program",
+	},
+	"Course": {
+		"before_validate": "eduedge.education.academic_validation.before_validate_course",
+	},
+	"Student Batch Name": {
+		"before_validate": "eduedge.education.academic_validation.before_validate_institution_owned_master",
+	},
+	"Student House": {
+		"before_validate": "eduedge.education.academic_validation.before_validate_institution_owned_master",
+	},
+	"Instructor": {
+		"before_validate": "eduedge.education.academic_validation.before_validate_institution_owned_master",
+	},
+	"Assessment Group": {
+		"before_validate": "eduedge.education.academic_validation.before_validate_institution_owned_master",
+	},
+	"Grading Scale": {
+		"before_validate": "eduedge.education.academic_validation.before_validate_institution_owned_master",
+	},
 	"Student Admission": {
 		"before_naming": "eduedge.education.branching.before_naming_student_admission",
 		"before_validate": "eduedge.education.branching.before_validate_student_admission",
@@ -68,6 +94,7 @@ doc_events = {
 	},
 	"Program Enrollment": {
 		"before_validate": "eduedge.education.branching.before_validate_program_enrollment",
+		"before_submit": "eduedge.education.enrollment_capacity.before_submit_program_enrollment",
 	},
 	"Student Group": {
 		"before_validate": "eduedge.education.branching.before_validate_student_group",
@@ -87,6 +114,21 @@ doc_events = {
 	"Assessment Result": {
 		"before_validate": "eduedge.education.assessment_operations.before_validate_assessment_result",
 	},
+	"Fee Structure": {
+		"before_validate": "eduedge.education.academic_validation.before_validate_fee_structure",
+	},
+	"Fee Schedule": {
+		"before_validate": "eduedge.education.academic_fee_context.before_validate_fee_schedule",
+	},
+	"Fees": {
+		"before_validate": "eduedge.education.academic_validation.before_validate_fees",
+	},
+	"Student Leave Application": {
+		"before_validate": "eduedge.education.academic_validation.before_validate_student_leave",
+	},
+	"Student Log": {
+		"before_validate": "eduedge.education.academic_validation.before_validate_student_log",
+	},
 	"EduEdge CBT Question": {
 		"validate": "eduedge.cbt.master_lifecycle.validate_master_docstatus",
 		"before_submit": "eduedge.cbt.master_lifecycle.block_master_submit",
@@ -100,6 +142,19 @@ doc_events = {
 }
 
 permission_query_conditions = {
+	"EduEdge Institution": "eduedge.education.institution_permissions.institution_query",
+	"EduEdge Academic Section": "eduedge.education.academic_permissions.academic_section_query",
+	"EduEdge Academic Level": "eduedge.education.academic_permissions.academic_level_query",
+	"EduEdge Institution Academic Calendar": "eduedge.education.academic_permissions.academic_calendar_query",
+	"Program": "eduedge.education.academic_permissions.program_query",
+	"Course": "eduedge.education.academic_permissions.course_query",
+	"Student Batch Name": "eduedge.education.academic_permissions.student_batch_query",
+	"Student House": "eduedge.education.academic_permissions.student_house_query",
+	"Instructor": "eduedge.education.academic_permissions.instructor_query",
+	"Assessment Group": "eduedge.education.academic_permissions.assessment_group_query",
+	"Grading Scale": "eduedge.education.academic_permissions.grading_scale_query",
+	"Fee Structure": "eduedge.education.academic_permissions.fee_structure_query",
+	"EduEdge Enrollment Status Log": "eduedge.education.academic_branch_permissions.enrollment_status_log_query",
 	"EduEdge School Branch": "eduedge.education.permissions.school_branch_query",
 	"Student Admission": "eduedge.education.permissions.student_admission_query",
 	"Student Applicant": "eduedge.education.permissions.student_applicant_query",
@@ -124,6 +179,19 @@ permission_query_conditions = {
 }
 
 has_permission = {
+	"EduEdge Institution": "eduedge.education.institution_permissions.has_institution_permission",
+	"EduEdge Academic Section": "eduedge.education.academic_permissions.has_academic_institution_permission",
+	"EduEdge Academic Level": "eduedge.education.academic_permissions.has_academic_institution_permission",
+	"EduEdge Institution Academic Calendar": "eduedge.education.academic_permissions.has_academic_institution_permission",
+	"Program": "eduedge.education.academic_permissions.has_academic_institution_permission",
+	"Course": "eduedge.education.academic_permissions.has_academic_institution_permission",
+	"Student Batch Name": "eduedge.education.academic_permissions.has_academic_institution_permission",
+	"Student House": "eduedge.education.academic_permissions.has_academic_institution_permission",
+	"Instructor": "eduedge.education.academic_permissions.has_academic_institution_permission",
+	"Assessment Group": "eduedge.education.academic_permissions.has_academic_institution_permission",
+	"Grading Scale": "eduedge.education.academic_permissions.has_academic_institution_permission",
+	"Fee Structure": "eduedge.education.academic_permissions.has_academic_institution_permission",
+	"EduEdge Enrollment Status Log": "eduedge.education.academic_branch_permissions.has_enrollment_status_log_permission",
 	"EduEdge School Branch": "eduedge.education.permissions.has_school_branch_record_permission",
 	"Student Admission": "eduedge.education.permissions.has_education_branch_permission",
 	"Student Applicant": "eduedge.education.permissions.has_education_branch_permission",
@@ -173,5 +241,5 @@ fixtures = [
 				],
 			]
 		],
-	}
+	},
 ]
