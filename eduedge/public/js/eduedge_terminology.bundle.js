5fb1e07eafe9c0c83ad056d0fe9e110793cc268b
olayemigod <olayemigod@gmail.com>
2026-07-22 18:19:10 +0100

    feat(eduedge): refresh runtime institution terminology context

--- eduedge/public/js/eduedge_terminology.bundle.js
+++ eduedge/public/js/eduedge_terminology.bundle.js
@@ -12,7 +12,12 @@ function getEduEdgeTerm(canonicalKey, options = {}) {
 function applyEduEdgeInstitutionContext(context = {}) {
 	frappe.boot = frappe.boot || {};
 	frappe.boot.eduedge_institution_context = context;
-	if (frappe.boot.eduedge_ui_identity) frappe.boot.eduedge_ui_identity.institution_context = context;
+	if (frappe.boot.eduedge_ui_identity) {
+		frappe.boot.eduedge_ui_identity.institution_context = context;
+		frappe.boot.eduedge_ui_identity.tenant_name = context.institution_name || context.company || frappe.boot.eduedge_ui_identity.tenant_name || "";
+		frappe.boot.eduedge_ui_identity.tenant_subtitle = context.institution_type_name || "Education workspace";
+	}
+	window.dispatchEvent(new CustomEvent("eduedge:institution-context-changed", { detail: context }));
 	return context;
 }
 
