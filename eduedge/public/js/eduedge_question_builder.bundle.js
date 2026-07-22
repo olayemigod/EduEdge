cef4ab97afcf903a47b76c70839ecc0b9e3356fe
olayemigod <olayemigod@gmail.com>
2026-07-22 03:15:21 +0100

    fix(eduedge): mount CBT rich text editor enhancer

--- eduedge/public/js/eduedge_question_builder.bundle.js
+++ eduedge/public/js/eduedge_question_builder.bundle.js
@@ -1,8 +1,37 @@
 import EduEdgeQuestionBuilder from "./eduedge_question_builder/EduEdgeQuestionBuilder.vue";
+import { installQuestionRichTextEditor } from "./eduedge_question_builder/rich_text_editor";
+import "./eduedge_question_builder/rich_text_editor.css";
 import { createEduEdgeApp } from "./eduedge_ui/app_factory";
 
+function attachQuestionRichTextEditor(viewModel) {
+	if (viewModel.$options?.name !== "EduEdgeQuestionBuilder") return;
+	viewModel.$nextTick(() => {
+		if (viewModel.__eduedgeQuestionRichTextEditor) {
+			const active = viewModel.__eduedgeQuestionRichTextEditor.refresh();
+			if (active) return;
+			viewModel.__eduedgeQuestionRichTextEditor.destroy();
+			viewModel.__eduedgeQuestionRichTextEditor = null;
+		}
+		viewModel.__eduedgeQuestionRichTextEditor = installQuestionRichTextEditor(viewModel.$el);
+	});
+}
+
 export function createEduEdgeQuestionBuilderApp(rootProps = null) {
-	return createEduEdgeApp(EduEdgeQuestionBuilder, rootProps);
+	const app = createEduEdgeApp(EduEdgeQuestionBuilder, rootProps);
+	app.mixin({
+		mounted() {
+			attachQuestionRichTextEditor(this);
+		},
+		updated() {
+			attachQuestionRichTextEditor(this);
+		},
+		beforeUnmount() {
+			if (this.$options?.name !== "EduEdgeQuestionBuilder") return;
+			this.__eduedgeQuestionRichTextEditor?.destroy();
+			this.__eduedgeQuestionRichTextEditor = null;
+		},
+	});
+	return app;
 }
 
 if (typeof window !== "undefined") {
