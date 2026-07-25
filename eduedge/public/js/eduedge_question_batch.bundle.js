a1081f1b2cb493d713a8298cd97e6678b8bf691e
olayemigod <olayemigod@gmail.com>
2026-07-25 16:43:45 +0100

    fix(eduedge): keep newest manual question card on top

--- eduedge/public/js/eduedge_question_batch.bundle.js
+++ eduedge/public/js/eduedge_question_batch.bundle.js
@@ -1,13 +1,36 @@
 import EduEdgeQuestionBatch from "./eduedge_question_batch/EduEdgeQuestionBatch.vue";
 import { createEduEdgeApp } from "./eduedge_ui/app_factory";
 
+const baseMethods = EduEdgeQuestionBatch.methods || {};
+const NewestFirstQuestionBatch = {
+	name: "EduEdgeQuestionBatch",
+	extends: EduEdgeQuestionBatch,
+	methods: {
+		addQuestion(...args) {
+			if (this.questions.length >= this.context.limits.manual_questions) return;
+			const before = this.questions.length;
+			const result = baseMethods.addQuestion.apply(this, args);
+			if (this.questions.length > before) {
+				const newest = this.questions.pop();
+				this.questions.unshift(newest);
+				this.$nextTick(() => {
+					this.$el
+						?.querySelector(".eduedge-question-card input.form-control")
+						?.focus();
+				});
+			}
+			return result;
+		},
+	},
+};
+
 export function createEduEdgeQuestionBatchApp(rootProps = null) {
-	return createEduEdgeApp(EduEdgeQuestionBatch, rootProps);
+	return createEduEdgeApp(NewestFirstQuestionBatch, rootProps);
 }
 
 if (typeof window !== "undefined") {
-	window.EduEdgeQuestionBatch = EduEdgeQuestionBatch;
+	window.EduEdgeQuestionBatch = NewestFirstQuestionBatch;
 	window.createEduEdgeQuestionBatchApp = createEduEdgeQuestionBatchApp;
 }
 
-export default EduEdgeQuestionBatch;
+export default NewestFirstQuestionBatch;
