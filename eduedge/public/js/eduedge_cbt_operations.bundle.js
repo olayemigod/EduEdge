37fbec09c0f7c4e104271db613a23d0c117bd94b
olayemigod <olayemigod@gmail.com>
2026-07-28 00:12:46 +0100

    feat(eduedge): route CBT question bank to EdgeSuite list

--- eduedge/public/js/eduedge_cbt_operations.bundle.js
+++ eduedge/public/js/eduedge_cbt_operations.bundle.js
@@ -1,13 +1,29 @@
 import EduEdgeCBTOperations from "./eduedge_cbt_operations/EduEdgeCBTOperations.vue";
 import { createEduEdgeApp } from "./eduedge_ui/app_factory";
+import { openEduEdgeRoute } from "./eduedge_ui/navigation";
+
+function resolveCBTOperationsRoute(route) {
+	return route === "/app/eduedge-cbt-question" ? "/app/eduedge-question-bank" : route;
+}
+
+const EduEdgeCBTOperationsPage = {
+	...EduEdgeCBTOperations,
+	methods: {
+		...(EduEdgeCBTOperations.methods || {}),
+		openRoute(route) {
+			return openEduEdgeRoute(resolveCBTOperationsRoute(route));
+		},
+	},
+};
 
 export function createEduEdgeCBTOperationsApp(rootProps = null) {
-	return createEduEdgeApp(EduEdgeCBTOperations, rootProps);
+	return createEduEdgeApp(EduEdgeCBTOperationsPage, rootProps);
 }
 
 if (typeof window !== "undefined") {
-	window.EduEdgeCBTOperations = EduEdgeCBTOperations;
+	window.EduEdgeCBTOperations = EduEdgeCBTOperationsPage;
 	window.createEduEdgeCBTOperationsApp = createEduEdgeCBTOperationsApp;
 }
 
-export default EduEdgeCBTOperations;
+export { resolveCBTOperationsRoute };
+export default EduEdgeCBTOperationsPage;
