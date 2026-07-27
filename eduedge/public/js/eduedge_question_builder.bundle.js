3b90aa0422986904d4491eccc75aa25ddcb216e0
olayemigod <olayemigod@gmail.com>
2026-07-27 14:49:48 +0100

    fix(eduedge): mount rich editor from question builder bundle

--- eduedge/public/js/eduedge_question_builder.bundle.js
+++ eduedge/public/js/eduedge_question_builder.bundle.js
@@ -1,6 +1,36 @@
 import EduEdgeQuestionBuilder from "./eduedge_question_builder/EduEdgeQuestionBuilder.vue";
-import "./eduedge_question_builder/rich_text_editor.css";
+import { installQuestionRichTextEditor } from "./eduedge_question_builder/rich_text_editor";
 import { createEduEdgeApp } from "./eduedge_ui/app_factory";
+
+const originalMounted = EduEdgeQuestionBuilder.mounted;
+const originalUpdated = EduEdgeQuestionBuilder.updated;
+const originalBeforeUnmount = EduEdgeQuestionBuilder.beforeUnmount;
+
+function ensureRichTextEditor(vm) {
+	vm.$nextTick(() => {
+		if (vm._eduedgeRichTextEditor?.refresh?.()) return;
+		vm._eduedgeRichTextEditor?.destroy?.();
+		vm._eduedgeRichTextEditor = installQuestionRichTextEditor(vm.$el);
+	});
+}
+
+EduEdgeQuestionBuilder.mounted = function mountedWithRichTextEditor(...args) {
+	const result = originalMounted?.apply(this, args);
+	ensureRichTextEditor(this);
+	return result;
+};
+
+EduEdgeQuestionBuilder.updated = function updatedWithRichTextEditor(...args) {
+	const result = originalUpdated?.apply(this, args);
+	ensureRichTextEditor(this);
+	return result;
+};
+
+EduEdgeQuestionBuilder.beforeUnmount = function beforeUnmountWithRichTextEditor(...args) {
+	this._eduedgeRichTextEditor?.destroy?.();
+	this._eduedgeRichTextEditor = null;
+	return originalBeforeUnmount?.apply(this, args);
+};
 
 export function createEduEdgeQuestionBuilderApp(rootProps = null) {
 	return createEduEdgeApp(EduEdgeQuestionBuilder, rootProps);
