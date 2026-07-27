c44f4697172017987caff6527c3a5cf70591c48f
olayemigod <olayemigod@gmail.com>
2026-07-27 10:48:37 +0100

    fix(eduedge): hide question upload without import permission

--- eduedge/public/js/eduedge_question_batch.bundle.js
+++ eduedge/public/js/eduedge_question_batch.bundle.js
@@ -2,6 +2,7 @@ import EduEdgeQuestionBatch from "./eduedge_question_batch/EduEdgeQuestionBatch.
 import { createEduEdgeApp } from "./eduedge_ui/app_factory";
 
 const originalAddQuestion = EduEdgeQuestionBatch.methods?.addQuestion;
+const originalLoadContext = EduEdgeQuestionBatch.methods?.loadContext;
 
 if (typeof originalAddQuestion === "function") {
 	EduEdgeQuestionBatch.methods.addQuestion = function addQuestionNewestFirst(...args) {
@@ -15,6 +16,20 @@ if (typeof originalAddQuestion === "function") {
 					?.querySelector(".eduedge-question-card input.form-control")
 					?.focus();
 			});
+		}
+		return result;
+	};
+}
+
+if (typeof originalLoadContext === "function") {
+	EduEdgeQuestionBatch.methods.loadContext = async function loadPermissionAwareContext(...args) {
+		const root = this.$el?.closest(".eduedge-question-batch-root");
+		root?.setAttribute("data-can-upload", "0");
+		const result = await originalLoadContext.apply(this, args);
+		const canUpload = Boolean(this.context?.can_upload);
+		root?.setAttribute("data-can-upload", canUpload ? "1" : "0");
+		if (!canUpload && this.mode === "upload") {
+			this.setMode("entry");
 		}
 		return result;
 	};
