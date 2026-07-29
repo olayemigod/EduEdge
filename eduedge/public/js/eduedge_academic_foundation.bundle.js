c0cc2f16cf365e38933e9fb758e23f84d810a4af
olayemigod <olayemigod@gmail.com>
2026-07-29 23:28:44 +0100

    fix(eduedge): install Academic Foundation QA corrections

--- eduedge/public/js/eduedge_academic_foundation.bundle.js
+++ eduedge/public/js/eduedge_academic_foundation.bundle.js
@@ -1,5 +1,8 @@
 import EduEdgeAcademicFoundation from "./eduedge_academic_foundation/EduEdgeAcademicFoundation.vue";
+import { installAcademicFoundationQaFixes } from "./eduedge_academic_foundation/qa_fixes";
 import { createEduEdgeApp } from "./eduedge_ui/app_factory";
+
+installAcademicFoundationQaFixes(EduEdgeAcademicFoundation);
 
 window.EduEdgeAcademicFoundation = EduEdgeAcademicFoundation;
 window.createEduEdgeAcademicFoundationApp = function createEduEdgeAcademicFoundationApp(props = {}) {
