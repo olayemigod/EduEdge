e1baa147caa2db1785dfc6867fc2486527f46dfc
olayemigod <olayemigod@gmail.com>
2026-07-30 01:54:44 +0100

    fix(eduedge): harden student group cascading context

--- eduedge/public/js/education/student_group.js
+++ eduedge/public/js/education/student_group.js
@@ -67,6 +67,26 @@ async function applyStudentGroupOffering(frm) {
 	}
 }
 
+async function clearStudentGroupContext(frm, fields, { clearInstructors = false } = {}) {
+	if (frm.__eduedge_applying_offering || frm.__eduedge_clearing_context) return;
+	frm.__eduedge_clearing_context = true;
+	try {
+		const values = {};
+		for (const fieldname of fields) {
+			if (frm.doc[fieldname]) values[fieldname] = null;
+		}
+		if (Object.keys(values).length) {
+			await frappe.model.set_value(frm.doctype, frm.docname, values);
+		}
+		frm.clear_table('students');
+		if (clearInstructors) frm.clear_table('instructors');
+		frm.refresh_fields(clearInstructors ? ['students', 'instructors'] : ['students']);
+		setStudentGroupQueries(frm);
+	} finally {
+		frm.__eduedge_clearing_context = false;
+	}
+}
+
 frappe.ui.form.on('Student Group', {
 	setup(frm) {
 		setStudentGroupQueries(frm);
@@ -74,7 +94,10 @@ frappe.ui.form.on('Student Group', {
 
 	refresh(frm) {
 		frm.set_df_property('eduedge_program_offering', 'label', frappe.eduedge?.term?.('programme_offering', { fallback: __('Programme Offering') }) || __('Programme Offering'));
-		frm.set_df_property('program', 'label', frappe.eduedge?.term?.('programme', { fallback: __('Program') }) || __('Program'));
+		frm.set_df_property('program', 'label', frappe.eduedge?.term?.('programme', { fallback: __('Programme') }) || __('Programme'));
+		frm.set_df_property('academic_year', 'label', frappe.eduedge?.term?.('academic_year', { fallback: __('Academic Session') }) || __('Academic Session'));
+		frm.set_df_property('academic_term', 'label', frappe.eduedge?.term?.('academic_term', { fallback: __('Academic Period') }) || __('Academic Period'));
+		frm.set_df_property('eduedge_academic_level', 'label', frappe.eduedge?.term?.('academic_level', { fallback: __('Academic Level') }) || __('Academic Level'));
 	},
 
 	onload(frm) {
@@ -89,12 +112,29 @@ frappe.ui.form.on('Student Group', {
 	},
 
 	eduedge_school_branch(frm) {
-		if (frm.__eduedge_applying_offering) return;
-		if (frm.doc.eduedge_program_offering) frm.set_value('eduedge_program_offering', null);
-		frm.set_value('program', null);
-		frm.clear_table('students');
-		frm.clear_table('instructors');
-		frm.refresh_fields(['students', 'instructors']);
-		setStudentGroupQueries(frm);
+		clearStudentGroupContext(
+			frm,
+			['eduedge_program_offering', 'eduedge_institution', 'program', 'academic_year', 'academic_term', 'batch', 'course', 'eduedge_academic_level'],
+			{ clearInstructors: true }
+		);
+	},
+
+	academic_year(frm) {
+		clearStudentGroupContext(frm, ['eduedge_program_offering', 'academic_term', 'eduedge_academic_level']);
+	},
+
+	academic_term(frm) {
+		clearStudentGroupContext(frm, ['eduedge_program_offering']);
+	},
+
+	program(frm) {
+		clearStudentGroupContext(frm, ['eduedge_program_offering', 'eduedge_academic_level']);
+	},
+
+	group_based_on(frm) {
+		clearStudentGroupContext(
+			frm,
+			['eduedge_program_offering', 'program', 'batch', 'course', 'eduedge_academic_level']
+		);
 	},
 });
