d87984ea3b7b97b19b825bb5911643fac06de6fe
olayemigod <olayemigod@gmail.com>
2026-07-30 03:42:00 +0100

    refactor(eduedge): remove legacy level cascade bundle

--- eduedge/public/js/eduedge_programme_offerings.bundle.js
+++ eduedge/public/js/eduedge_programme_offerings.bundle.js
@@ -1,18 +1,13 @@
 import EduEdgeProgrammeOfferings from "./eduedge_programme_offerings/EduEdgeProgrammeOfferings.vue";
-import { applyProgrammeOfferingLevelCascade } from "./eduedge_programme_offerings/level_cascade";
-import { applyProgrammeOfferingTerminology } from "./eduedge_programme_offerings/terminology";
 import { createEduEdgeApp } from "./eduedge_ui/app_factory";
 
-const CascadedProgrammeOfferings = applyProgrammeOfferingLevelCascade(EduEdgeProgrammeOfferings);
-const TerminologyAwareProgrammeOfferings = applyProgrammeOfferingTerminology(CascadedProgrammeOfferings);
-
 export function createEduEdgeProgrammeOfferingsApp(rootProps = null) {
-	return createEduEdgeApp(TerminologyAwareProgrammeOfferings, rootProps);
+	return createEduEdgeApp(EduEdgeProgrammeOfferings, rootProps);
 }
 
 if (typeof window !== "undefined") {
-	window.EduEdgeProgrammeOfferings = TerminologyAwareProgrammeOfferings;
+	window.EduEdgeProgrammeOfferings = EduEdgeProgrammeOfferings;
 	window.createEduEdgeProgrammeOfferingsApp = createEduEdgeProgrammeOfferingsApp;
 }
 
-export default TerminologyAwareProgrammeOfferings;
+export default EduEdgeProgrammeOfferings;
