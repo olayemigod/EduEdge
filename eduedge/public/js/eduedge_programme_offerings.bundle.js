00bdcefde2f5659bdb6a7c496748a9d4991ee09f
olayemigod <olayemigod@gmail.com>
2026-07-30 02:03:46 +0100

    fix(eduedge): apply session terminology to offering page

--- eduedge/public/js/eduedge_programme_offerings.bundle.js
+++ eduedge/public/js/eduedge_programme_offerings.bundle.js
@@ -1,16 +1,18 @@
 import EduEdgeProgrammeOfferings from "./eduedge_programme_offerings/EduEdgeProgrammeOfferings.vue";
 import { applyProgrammeOfferingLevelCascade } from "./eduedge_programme_offerings/level_cascade";
+import { applyProgrammeOfferingTerminology } from "./eduedge_programme_offerings/terminology";
 import { createEduEdgeApp } from "./eduedge_ui/app_factory";
 
 const CascadedProgrammeOfferings = applyProgrammeOfferingLevelCascade(EduEdgeProgrammeOfferings);
+const TerminologyAwareProgrammeOfferings = applyProgrammeOfferingTerminology(CascadedProgrammeOfferings);
 
 export function createEduEdgeProgrammeOfferingsApp(rootProps = null) {
-	return createEduEdgeApp(CascadedProgrammeOfferings, rootProps);
+	return createEduEdgeApp(TerminologyAwareProgrammeOfferings, rootProps);
 }
 
 if (typeof window !== "undefined") {
-	window.EduEdgeProgrammeOfferings = CascadedProgrammeOfferings;
+	window.EduEdgeProgrammeOfferings = TerminologyAwareProgrammeOfferings;
 	window.createEduEdgeProgrammeOfferingsApp = createEduEdgeProgrammeOfferingsApp;
 }
 
-export default CascadedProgrammeOfferings;
+export default TerminologyAwareProgrammeOfferings;
