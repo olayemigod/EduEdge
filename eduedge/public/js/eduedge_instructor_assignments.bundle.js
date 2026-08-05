8ac1482bdc3e57a894d0fb34c13eaa96126dba8c
olayemigod <olayemigod@gmail.com>
2026-08-05 17:53:54 +0100

    fix(eduedge): keep newest assignment row on top

--- eduedge/public/js/eduedge_instructor_assignments.bundle.js
+++ eduedge/public/js/eduedge_instructor_assignments.bundle.js
@@ -1,6 +1,33 @@
 import EduEdgeInstructorAssignments from "./eduedge_instructor_assignments/EduEdgeInstructorAssignments.vue";
 import { createEduEdgeApp } from "./eduedge_ui/app_factory";
 
+function keepNewestAssignmentRowOnTop(methodName) {
+	const methods = EduEdgeInstructorAssignments.methods || {};
+	const original = methods[methodName];
+	if (typeof original !== "function") return;
+
+	methods[methodName] = function (...args) {
+		const existingIds = new Set((this.rows || []).map((row) => row.row_id));
+		const result = original.apply(this, args);
+		const createdIndex = (this.rows || []).findIndex((row) => !existingIds.has(row.row_id));
+		if (createdIndex > 0) {
+			const [newest] = this.rows.splice(createdIndex, 1);
+			this.rows.unshift(newest);
+		}
+		this.$nextTick?.(() => {
+			document.querySelector(".rows-stack .assignment-row")?.scrollIntoView({
+				behavior: "smooth",
+				block: "start",
+			});
+		});
+		return result;
+	};
+}
+
+keepNewestAssignmentRowOnTop("addAcademicRow");
+keepNewestAssignmentRowOnTop("addBranchAccessRow");
+keepNewestAssignmentRowOnTop("duplicateRow");
+
 export function createEduEdgeInstructorAssignmentsApp(rootProps = null) {
 	return createEduEdgeApp(EduEdgeInstructorAssignments, rootProps);
 }
