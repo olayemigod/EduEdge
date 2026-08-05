41ee46ab4844ed3a3b2da54470bcb275ee47d11a
olayemigod <olayemigod@gmail.com>
2026-08-05 12:41:04 +0100

    refactor(eduedge): normalize Instructor Assignment menu label

--- eduedge/public/js/eduedge_instructors.bundle.js
+++ eduedge/public/js/eduedge_instructors.bundle.js
@@ -1,7 +1,21 @@
 import EduEdgeInstructors from "./eduedge_instructors/EduEdgeInstructors.vue";
 import { createEduEdgeApp } from "./eduedge_ui/app_factory";
+import { EDUEDGE_MENU_ITEMS } from "./eduedge_ui/navigation";
+
+function normalizeInstructorAssignmentMenu() {
+	for (const group of EDUEDGE_MENU_ITEMS || []) {
+		for (const item of group.items || []) {
+			if (item.route !== "/app/eduedge-instructor-assignments") continue;
+			item.label = __("Instructor Assignments");
+			item.description = __("Assign Instructors across Institutions, Branches, Classes, Class Arms, and Subjects");
+		}
+	}
+}
+
+normalizeInstructorAssignmentMenu();
 
 export function createEduEdgeInstructorsApp(rootProps = null) {
+	normalizeInstructorAssignmentMenu();
 	return createEduEdgeApp(EduEdgeInstructors, rootProps);
 }
 
