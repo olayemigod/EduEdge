8bc4ef92b6d9c992f84c8a97542baaa214b06d30
olayemigod <olayemigod@gmail.com>
2026-08-07 01:49:25 +0100

    feat(eduedge): wire curriculum governance controls into programmes page

--- eduedge/public/js/eduedge_programmes.bundle.js
+++ eduedge/public/js/eduedge_programmes.bundle.js
@@ -1,8 +1,16 @@
 import EduEdgeProgrammes from "./eduedge_programmes/EduEdgeProgrammes.vue";
+import { installProgrammeCurriculumGovernance } from "./eduedge_programmes/curriculum_governance";
 import { createEduEdgeApp } from "./eduedge_ui/app_factory";
 
 export function createEduEdgeProgrammesApp(rootProps = null) {
-	return createEduEdgeApp(EduEdgeProgrammes, rootProps);
+	const app = createEduEdgeApp(EduEdgeProgrammes, rootProps);
+	const originalMount = app.mount.bind(app);
+	app.mount = (root) => {
+		const result = originalMount(root);
+		installProgrammeCurriculumGovernance(app, root);
+		return result;
+	};
+	return app;
 }
 
 if (typeof window !== "undefined") {
