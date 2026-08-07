e09c976d407c3ae8b0f8272e0fbfe95b11a65889
olayemigod <olayemigod@gmail.com>
2026-08-07 02:22:51 +0100

    fix(eduedge): activate class modal save correction

--- eduedge/public/js/eduedge_programmes.bundle.js
+++ eduedge/public/js/eduedge_programmes.bundle.js
@@ -1,5 +1,6 @@
 import EduEdgeProgrammes from "./eduedge_programmes/EduEdgeProgrammes.vue";
 import { installProgrammeCurriculumGovernance } from "./eduedge_programmes/curriculum_governance";
+import { installProgrammeModalSaveFix } from "./eduedge_programmes/programme_modal_save_fix";
 import { createEduEdgeApp } from "./eduedge_ui/app_factory";
 
 export function createEduEdgeProgrammesApp(rootProps = null) {
@@ -7,6 +8,7 @@ export function createEduEdgeProgrammesApp(rootProps = null) {
 	const originalMount = app.mount.bind(app);
 	app.mount = (root) => {
 		const proxy = originalMount(root);
+		installProgrammeModalSaveFix(proxy);
 		installProgrammeCurriculumGovernance(app, root, proxy);
 		return proxy;
 	};
