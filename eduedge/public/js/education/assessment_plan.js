db5919a7f1fb18eeb0c24e486a059b0918d4013e
olayemigod <olayemigod@gmail.com>
2026-08-10 23:55:25 +0100

    feat(eduedge): cascade assessment plan options through assignment capability queries

--- eduedge/public/js/education/assessment_plan.js
+++ eduedge/public/js/education/assessment_plan.js
@@ -1,11 +1,20 @@
 frappe.ui.form.on("Assessment Plan", {
 	setup(frm) {
 		frm.set_query("student_group", () => ({
-			query: "eduedge.api.academic_operations.student_group_query",
+			query: "eduedge.api.assessment_assignment_options.assessment_plan_student_group_query",
 			filters: {
 				eduedge_school_branch: frm.doc.eduedge_school_branch,
 				academic_year: frm.doc.academic_year,
 				academic_term: frm.doc.academic_term,
+				schedule_date: frm.doc.schedule_date,
+			},
+		}));
+		frm.set_query("course", () => ({
+			query: "eduedge.api.assessment_assignment_options.assessment_plan_course_query",
+			filters: {
+				eduedge_school_branch: frm.doc.eduedge_school_branch,
+				student_group: frm.doc.student_group,
+				schedule_date: frm.doc.schedule_date,
 			},
 		}));
 		frm.set_query("room", () => ({
@@ -21,12 +30,30 @@ frappe.ui.form.on("Assessment Plan", {
 			}));
 		}
 	},
+	eduedge_school_branch(frm) {
+		frm.set_value("student_group", null);
+		frm.set_value("course", null);
+		frm.set_value("room", null);
+		frm.set_value("examiner", null);
+		frm.set_value("supervisor", null);
+	},
+	academic_year(frm) {
+		frm.set_value("student_group", null);
+		frm.set_value("course", null);
+	},
+	academic_term(frm) {
+		frm.set_value("student_group", null);
+		frm.set_value("course", null);
+	},
 	student_group(frm) {
+		frm.set_value("course", null);
 		frm.set_value("room", null);
 		frm.set_value("examiner", null);
 		frm.set_value("supervisor", null);
 	},
 	schedule_date(frm) {
+		frm.set_value("student_group", null);
+		frm.set_value("course", null);
 		frm.set_value("examiner", null);
 		frm.set_value("supervisor", null);
 	},
