8776a3098850199f13a4a2b5587dffd89ffb7ad8
olayemigod <olayemigod@gmail.com>
2026-08-14 01:04:05 +0100

    fix(eduedge): load EdgeSuite lifecycle modal bridge

--- eduedge/public/js/eduedge_instructor_assignments/replacement_dialog.js
+++ eduedge/public/js/eduedge_instructor_assignments/replacement_dialog.js
@@ -1,3 +1,4 @@
+import "../eduedge_instructor_assignment_end.bundle.js";
 import InstructorAssignmentReplacementDialog from "../eduedge_ui/components/InstructorAssignmentReplacementDialog.vue";
 import { createEduEdgeApp } from "../eduedge_ui/app_factory";
 
