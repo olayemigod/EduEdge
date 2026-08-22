36a920bbc9c9c4800c15443c58d1fcfeeba27193
olayemigod <olayemigod@gmail.com>
2026-08-22 22:04:01 +0100

    fix(eduedge): activate Scheme Session Launch context

--- eduedge/public/js/eduedge_scheme_of_work.bundle.js
+++ eduedge/public/js/eduedge_scheme_of_work.bundle.js
@@ -1,5 +1,8 @@
 import EduEdgeSchemeOfWork from "./eduedge_scheme_of_work/EduEdgeSchemeOfWork.vue";
+import { installSchemeSessionContext } from "./eduedge_scheme_of_work/session_context";
 import { createEduEdgeApp } from "./eduedge_ui/app_factory";
+
+installSchemeSessionContext(EduEdgeSchemeOfWork);
 
 export function createEduEdgeSchemeOfWorkApp(rootProps = null) {
 	return createEduEdgeApp(EduEdgeSchemeOfWork, rootProps);
