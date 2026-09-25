f33ed6b22ee6f2fcb424d19a95bf0b8c7354f61b
olayemigod <olayemigod@gmail.com>
2026-09-25 16:06:48 +0100

    fix(eduedge): use mark-entry Assessment Plan selector

--- eduedge/public/js/education/assessment_result.js
+++ eduedge/public/js/education/assessment_result.js
@@ -1,9 +1,9 @@
 frappe.ui.form.on("Assessment Result", {
 	setup(frm) {
 		frm.set_query("assessment_plan", () => ({
+			query: "eduedge.api.assessment_assignment_options.assessment_result_plan_query",
 			filters: {
 				eduedge_school_branch: frm.doc.eduedge_school_branch,
-				docstatus: 1,
 			},
 		}));
 		frm.set_query("student", () => ({
