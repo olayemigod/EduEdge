1846e31a27a0393eea85e7da84f875f89d925594
olayemigod <olayemigod@gmail.com>
2026-09-26 08:21:01 +0100

    test(eduedge): allow governed assessment criteria override

--- eduedge/tests/test_program_offering_contract.py
+++ eduedge/tests/test_program_offering_contract.py
@@ -51,6 +51,7 @@ class TestProgramOfferingContract(unittest.TestCase):
 			{
 				"education.education.doctype.student_attendance_tool.student_attendance_tool.get_student_attendance_records",
 				"education.education.api.mark_attendance",
+				"education.education.api.get_assessment_criteria",
 				"education.education.api.get_assessment_students",
 				"education.education.api.get_assessment_details",
 				"education.education.api.mark_assessment_result",
